0ea6c3d917a967ea7dc1a5ecda5c236f82e0d7bf
Daryl <dtatton89@gmail.com>
2018-01-13 12:34:20 -0400

    Initial optimized XML Layout added new icons to main menu

--- app/src/main/java/com/example/daryl/shinigamifanapp/MainActivity.java
+++ app/src/main/java/com/example/daryl/shinigamifanapp/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.daryl.shinigamifanapp;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebView;
+import android.widget.ImageView;
 
 import java.io.BufferedInputStream;
 import java.io.InputStream;
@@ -14,20 +16,41 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       // Uri.Builder b = Uri.parse("http://www.twitch.tv/Red_Shinigami89").buildUpon();
+        // Uri.Builder b = Uri.parse("http://www.twitch.tv/Red_Shinigami89").buildUpon();
 
     }
+    //ImageView img =(ImageView)findViewById(R.id.discord_icon);
 
-            public void onClick(View view){
+    public void discordLink(View view) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+        intent.setData(Uri.parse("https://discord.gg/Svt5nTh"));
+        startActivity(intent);
+    }
 
-             setContentView(R.layout.discord);
+    public void twitterLink(View view) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+        intent.setData(Uri.parse("https://www.twitter.com/red_shinigami89"));
+        startActivity(intent);
+
+    }
+    public void twitchLink(View view) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+        intent.setData(Uri.parse("https://www.twitch.tv/red_shinigami89"));
+        startActivity(intent);
+    }
 }
 
 
 
-}
 
